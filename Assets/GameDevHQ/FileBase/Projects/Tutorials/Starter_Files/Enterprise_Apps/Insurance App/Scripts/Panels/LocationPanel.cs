f0fe3a5865b6546ab54731f3dbf81af422b22925
adavis1989 <83476184+adavis1989@users.noreply.github.com>
2022-02-17 21:28:46 -0600

    Created the client info panel functions

--- Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Enterprise_Apps/Insurance App/Scripts/Panels/LocationPanel.cs
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Enterprise_Apps/Insurance App/Scripts/Panels/LocationPanel.cs
@@ -5,10 +5,16 @@ using UnityEngine.UI;
 
 public class LocationPanel : MonoBehaviour, IPanel
 {
+    public Text caseNumberText;
     public RawImage map;
     public InputField mapNotes;
 
+    public void OnEnable()
+    {
+        caseNumberText.text = "CASE NUMBER 000" + UIManager.Instance.activeCase.caseID;
+    }
     public void ProcessInfo()
     {
+
     }
 }
