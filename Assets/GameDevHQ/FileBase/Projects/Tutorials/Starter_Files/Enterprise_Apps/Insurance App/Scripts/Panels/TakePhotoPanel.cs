e831829c0705ccd7a24b8edd0f450a059f784ab7
adavis1989 <83476184+adavis1989@users.noreply.github.com>
2022-03-03 19:20:37 -0600

    Finished the 'Take A Photo' Section!

--- Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Enterprise_Apps/Insurance App/Scripts/Panels/TakePhotoPanel.cs
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Enterprise_Apps/Insurance App/Scripts/Panels/TakePhotoPanel.cs
@@ -8,6 +8,7 @@ public class TakePhotoPanel : MonoBehaviour, IPanel
     public RawImage photoTaken;
     public InputField photoNotes;
     public Text caseNumberText;
+	public GameObject overViewPanel;
 
     public void OnEnable()
     {
@@ -16,5 +17,35 @@ public class TakePhotoPanel : MonoBehaviour, IPanel
 
     public void ProcessInfo()
     {
+		UIManager.Instance.activeCase.photoTaken = photoTaken.texture;
+		UIManager.Instance.activeCase.photoNotes = photoNotes.text;
+		overViewPanel.SetActive(true);
     }
+
+	public void TakePictureButton()
+    {
+		TakePicture(512);
+    }
+    private void TakePicture(int maxSize)
+	{
+		NativeCamera.Permission permission = NativeCamera.TakePicture((path) =>
+		{
+			Debug.Log("Image path: " + path);
+			if (path != null)
+			{
+				// Create a Texture2D from the captured image
+				Texture2D texture = NativeCamera.LoadImageAtPath(path, maxSize);
+				if (texture == null)
+				{
+					Debug.Log("Couldn't load texture from " + path);
+					return;
+				}
+
+				photoTaken.texture = texture;
+				photoTaken.gameObject.SetActive(true);
+			}
+		}, maxSize);
+
+		Debug.Log("Permission result: " + permission);
+	}
 }
